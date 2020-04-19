6e25f3e5f585d1feda5b4d5eb7e76c463d8374ce
Bill Z. Xing <bill.z.xing@gmail.com>
2020-04-19 15:19:03 -0400

    One step closer

--- source.py
+++ source.py
@@ -1,17 +1,47 @@
 #!/usr/bin/python3
 import json
+from math import ceil
+
+
+def print_kv(k, v):
+    print(f'{k:20} => {v:50d}')
+
+
+def wrap_around(start: int, step: int, target: int) -> (int, int):
+    print_kv("start", start)
+    print_kv("step", step)
+    num_steps, quotient = divmod(target, step)
+    if quotient * 2 > step:
+        num_steps += 1
+    new_start: int = (start + num_steps * step) % target
+    return num_steps, new_start
 
 
 def solve_x(s: int, k: int, h: int, r: int, q: int) -> int:
-    n: int
-    remainder = (k * s - h) % r
-    print(remainder)
+    print_kv("r", r)
 
-    for n in range(1, 15):
-        remainder = (remainder + (k * q)) % r
-        print(remainder)
+    start: int = (k * s - h) % r
 
-    return 0
+    step: int = (k * q) % r
+
+    n: int = 1
+    i: int = 0
+    while start != 0 and i < 100:
+        if step == 0:
+            raise RuntimeError("Hit a wall...")
+        num_steps, new_start = wrap_around(start, step, r)
+        new_step = new_start - start
+
+        step = new_step
+        start = new_start
+        n *= num_steps
+
+        i += 1
+
+    x, remainder = divmod((k * (s + n * q) - h), r)
+    if remainder != 0:
+        raise RuntimeError("Result is wrong")
+    return x
 
 
 def solve_k(r: int, g: int, p: int, q: int, k_range: range) -> int:
@@ -37,10 +67,10 @@ def main():
     s: int = input_object["s"]
 
     k: int = solve_k(r, g, p, q, range(1, 65536))
-    print(k)
+    print_kv("k", k)
 
     x: int = solve_x(s, k, h, r, q)
-    print(x)
+    print_kv("x", x)
 
     return 0
 
