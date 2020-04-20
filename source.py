38cd34c91af2b39969b6295a5569cdb7fc516fed
Bill Z. Xing <bill.z.xing@gmail.com>
2020-04-19 20:10:57 -0400

    Add back everything that didnt work so far

--- source.py
+++ source.py
@@ -1,10 +1,10 @@
-#!/usr/bin/python3
+#!/usr/bin/python3.8
 import json
-from math import ceil
+import math
 
 
 def print_kv(k, v):
-    print(f'{k:20} => {v:50d}')
+    print(f'{k:20} => {v:80d}')
 
 
 def wrap_around(start: int, step: int, target: int) -> (int, int):
@@ -15,14 +15,102 @@ def wrap_around(start: int, step: int, target: int) -> (int, int):
     return num_steps, new_start
 
 
-def solve_x(s: int, k: int, h: int, r: int, q: int) -> int:
+def lcm(a: int, b: int) -> int:
+    return a * b // math.gcd(a, b)
+
+
+def solve_x3(s: int, k: int, h: int, r: int, q: int) -> int:
+    print_kv("r", r)
+
+    start: int = (k * s - h) % r
+
+    step: int = (k * q) % r
+
+    n: int = 1
+    i: int = 0
+    while start != 0 and i < 100:
+        print_kv("start", start)
+        if step == 0:
+            raise RuntimeError("Hit a wall...")
+        num_steps, new_start = wrap_around(start, step, r)
+        new_step = new_start - start
+        print_kv("new_step", new_step)
+
+        step = new_step
+        start = new_start
+        n *= num_steps
+
+        i += 1
+
+    x, remainder = divmod((k * (s + n * q) - h), r)
+    if remainder != 0:
+        raise RuntimeError("Result is wrong")
+    return x
+
+
+def solve_x2(s: int, k: int, h: int, r: int, q: int) -> int:
+    print_kv("r", r)
+
+    start: int = (k * s - h) % r
+
+    step: int = (k * q) % r
+
+    n: int = 1
+    i: int = 0
+    while start != 0 and i < 50000:
+        print_kv("i", i)
+        print_kv("start", start)
+        print_kv("step", step)
+
+        if step == 0:
+            print_kv("n", n)
+            raise RuntimeError("Hit a wall...")
+
+        num_steps, new_start = wrap_around(start, step, r)
+        new_step = (new_start - start) % step
+
+        if step <= new_step:
+            raise RuntimeError("It's not converging..")
+        step = new_step
+        start = new_start
+        n *= num_steps
+        print_kv("num_steps", num_steps)
+        print_kv("n", n)
+
+        current_remainder = (k * s + k * n * q - h) % r
+        print_kv("current_remainder", current_remainder)
+        print_kv("new_start % r", new_start % r)
+
+        i += 1
+
+    x, remainder = divmod((k * (s + n * q) - h), r)
+    if remainder != 0:
+        raise RuntimeError("Result is wrong")
+    return x
+
+
+def solve_x1(s: int, k: int, h: int, r: int, q: int) -> int:
     # constraint to solve:
     # (x, 0) = divmod(ks - h + nkq , r)
+    kq = k * q
+    offset = (k * s - h)
+    print_kv("q", q)
+    print_kv("r", r)
+    print_kv("kq", kq)
+    print_kv("offset", offset)
     m = 0
     n = 249018150293514463697521504033785680812009408931 \
         + m * 455326676787712335755587942590900495631551334913
-    x, remainder = divmod((k * (s + n * q) - h), r)
+    print_kv("n", n)
+    x, remainder = divmod((offset + n * kq), r)
+    if remainder != 0:
+        raise RuntimeError("Wrong x - cannot divide cleanly")
+
     return x
+
+
+def solve_x(s: int, k: int, h: int, r: int, q: int) -> int:
+    return 0
 
 
 def solve_k(r: int, g: int, p: int, q: int, k_range: range) -> int:
@@ -35,14 +123,13 @@ def solve_k(r: int, g: int, p: int, q: int, k_range: range) -> int:
 
 def sign(k: int, g: int, p: int, q: int, h: int, x: int) -> (int, int):
     r: int = pow(g, k, p) % q
-    a, b = divmod((h + x * r), k)
-    if b != 0:
-        raise RuntimeError("Cannot divide nicely")
-    s: int = a % q
+    k_inverse: int = pow(k, -1, q)
+    s: int = (h + x * r) * k_inverse % q
     return r, s
 
 
 def main():
+    # Phase 1: Read Input
     with open("zxing7_input.json") as fp:
         input_object = json.load(fp)
     print(json.dumps(input_object, indent=4))
@@ -56,12 +143,14 @@ def main():
     r: int = input_object["r"]
     s: int = input_object["s"]
 
+    # Phase 2: Solve
     k: int = solve_k(r, g, p, q, range(1, 65536))
     print_kv("k", k)
 
-    x: int = solve_x(s, k, h, r, q)
+    x: int = solve_x2(s, k, h, r, q)
     print_kv("x", x)
 
+    # Phase 3: Verify
     r_out, s_out = sign(k, g, p, q, h, x)
     print_kv("r_out", r_out)
     print_kv("s_out", s_out)
